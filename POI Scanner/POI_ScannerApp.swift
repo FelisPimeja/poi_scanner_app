290303c11fa5a4e632fad4cfa75553802ea13a86
Aleksandr Petrov <petroval@whoosh.bike>
2026-04-18 18:26:00 +0300

    Initial Commit

---
+++ POI Scanner/POI_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  POI_ScannerApp.swift
+//  POI Scanner
+//
+//  Created by Aleksandr Petrov on 18.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct POI_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
